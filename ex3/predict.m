543003ff48f2e3c31ed41b21836f2432125d4b14
fernandocardenasm <fernando.cardenasm@gmail.com>
2016-05-31 22:24:27 +0200

    Add Prediction with Neuronal Networks

--- ex3/predict.m
+++ ex3/predict.m
@@ -10,6 +10,8 @@ num_labels = size(Theta2, 1);
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
+X = [ones(m, 1) X];
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
 %               your learned neural network. You should set p to a 
@@ -23,9 +25,15 @@ p = zeros(size(X, 1), 1);
 
 
 
+a2 = sigmoid(X * Theta1');
 
+a2 = [ones(m, 1) a2];
 
+hx = sigmoid(a2 * Theta2');
 
+[max_value, max_index] = max(hx, [], 2);
+
+p = max_index;
 
 
 
